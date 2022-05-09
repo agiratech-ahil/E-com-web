23014e0d857a2e6dde32f9181a0741373ca99541
agiratech-ahil <102363021+agiratech-ahil@users.noreply.github.com>
2022-05-09 16:28:54 +0530

    updated
    new changes in js

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ window.onload = function () {
   //adding data to local storage
 
   const addTo = document.getElementsByClassName("add");
+
   const cart = document.querySelector(".cartbox");
 
   var items = [];
   for (let i = 0; i < addTo.length; i++) {
     addTo[i].addEventListener("click", function (e) {
       if (typeof Storage !== "undefined") {
-        let item = {
+        var item = {
           id: i + 1,
           model: e.target.parentElement.children[1].textContent,
           price: e.target.parentElement.children[3].textContent,
@@ -35,12 +36,34 @@ window.onload = function () {
       }
     });
   }
+
+  //Store and display no. of. items
+  const scart = document.querySelector(".count p");
+  let no = 0;
+  JSON.parse(localStorage.getItem("products")).map((data) => {
+    no = no + data.no;
+  });
+
+  scart.innerHTML = no;
+  //Store and display no. of. items
+  const totalp = document.querySelector(".price p");
+  var p = 0;
+  var str = JSON.parse(localStorage.getItem("products"));
+  var s = str.map((item) => item.price * item.no);
+
+  for (let i = 0; i < s.length; i++) {
+    p += s[i];
+  }
+
+  console.log(p);
+  totalp.innerHTML = "₹" + p;
+
   // adding data to the table
-  const carttable = cart.querySelector("table");
+  var carttable = cart.querySelector("table");
 
   let tabledata = "";
   tabledata +=
-    "<tr><th>Item Name</th><th>No of Items</th><th>Item Price</th></tr>";
+    "<tr><th>Item Name</th><th>No of Items</th><th>Item Price</th><th>Total Price</th></tr>";
 
   if (JSON.parse(localStorage.getItem("products")) === null) {
     tabledata += '<tr><td colspan="4"> No items found<td></tr>';
@@ -51,9 +74,12 @@ window.onload = function () {
         </td><td id='number'> 
         ${data.no} 
         </td><td id = 'rate'>
+        ₹${data.price} 
+        </td><td >
         ₹${data.price * data.no} 
         </td></tr>`;
     });
   }
   carttable.innerHTML = tabledata;
 };
+
